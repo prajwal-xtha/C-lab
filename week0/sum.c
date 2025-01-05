71abb34fb24cef05511a6e40f5569edecb6e8f8d
hiraniroula <130172491+HiraNiroula@users.noreply.github.com>
2025-01-05 23:09:41 +0545

    sum

---
+++ week0/sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("\nEnter First Number : ");
+    scanf("%d",&a);
+    printf("\nEnter Second Number : ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("THE sum of %d + %d is %d",a,b,c);
+    return 0;
+}
